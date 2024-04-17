4ff82d0455753709d49dcbcd20693d0ab0df85fe
Siva837453 <sivaallaka777@gmail.com>
2024-04-17 12:20:06 +0530

    variables script

--- array.sh
+++ array.sh
@@ -2,6 +2,6 @@
 
 MOVIES=("RRR" "SALAR" "DJ TILLU")
 
-echo "first prize is : {$MOVIES[0]}"
-echo "Fsecond prize is : {$MOVIES[1]}"
-echo "all: {$MOVIES[@]}"
+echo "first prize is : ${MOVIES[0]}"
+echo "Fsecond prize is : ${MOVIES[1]}"
+echo "all: ${MOVIES[@]}"
