419b38be3e473346f7255e59d1a1fbbd7347780e
Siva837453 <sivaallaka777@gmail.com>
2024-04-21 07:44:03 +0530

    data types

--- conditions.sh
+++ conditions.sh
@@ -2,11 +2,11 @@
 
 NUMBER=$1
 
-if [$NUMBER -gt 10]
+if [ $NUMBER -gt 10 ]
 then
-echo "given $NUMBER is greater than 10"
+    echo "given $NUMBER is greater than 10"
 else
-echo "given $NUMBER is less than 10"
+    echo "given $NUMBER is less than 10"
 
 fi
 
