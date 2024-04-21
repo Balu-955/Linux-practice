b2db1e283e59fcd103be4688286b616ae32d6661
Siva837453 <sivaallaka777@gmail.com>
2024-04-21 10:05:01 +0530

    data types

---
+++ installation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if (USERID -ne 0)
+then
+    echo "please user super user to run command"
+    exit 1 #manually exit if error comes
+else
+    echo "you are super user"
+ 
