8a66cb900f0e8d99b9fde436f6d5e63e18669071
Siva837453 <sivaallaka777@gmail.com>
2024-04-21 07:36:40 +0530

    data types

--- conditions.sh
+++ conditions.sh
@@ -5,9 +5,9 @@ NUMBER=$1
 if [$NUMBER -gt 10]
 
 echo "given number is greater than 10"
-
-else [$NUMBER -lt 10]
-
+else
 echo "given number is less than 10"
 
 fi
+
+#-gt(greater than)  -lt(less than) -eq(equal to) -ge(greater that equal to) -le)less than equal to)
