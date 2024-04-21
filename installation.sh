c97ed06e0c5d1d82fecbcd437d64f0719483fcf6
Siva837453 <sivaallaka777@gmail.com>
2024-04-21 10:09:56 +0530

    data types

--- installation.sh
+++ installation.sh
@@ -2,10 +2,10 @@
 
 USERID=$(id -u)
 
-if (USERID -ne 0)
+if [ $USERID -ne 0 ]
 then
-    echo "please user super user to run command"
+    echo "please use super user to run command"
     exit 1 #manually exit if error comes
-else
-    echo "you are super user"
+#else
+#   echo "you are super user"
  fi
