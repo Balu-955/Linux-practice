c2f682f737f662f93e2b3f7f70255c72e6af045d
Siva837453 <sivaallaka777@gmail.com>
2024-04-22 18:22:51 +0530

    data types

--- 11.functions.sh
+++ 11.functions.sh
@@ -11,11 +11,14 @@ VALIDATE(){
 if [ $USERID -ne 0 ]
 then
     echo "please use super user to run command"
+    exit 1
 else
     echo " You are super user"
 fi
 
 dnf install mysql -y
+VALIDATE $? "insatll mysql"
 
 
 dnf install git -y
+VALIDATE $? "install git"
