9f2ac72e818a09cbad4c79cc7faae9004322ce6c
Siva837453 <sivaallaka777@gmail.com>
2024-04-24 08:37:18 +0530

    data types

---
+++ 13.packages_install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +f-%H-%M-%S)
+SCRIPTNAME=$($0 | cut -d"." -f1)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE
+if [ $1 -ne 0 ]
+then 
+    echo -e " installing ...$R FAILURE $N"
+else
+    echo " installing...$G success $N"
+fi
+
+if [ $USERID -ne 0 ]
+then
+    echo "please use super user to run command"
+else
+    echo "you are super user"
+fi
+
+
+for i in $@
+do
+    echo "packages to installed:  $i"
+    dnf list installed $i &>>$LOGFILE
+
+    if [ $? -eq 0]
+    then
+        echo "$i alreday installed...$G SKIPPED $N"
+    else
+        echo " $i not installed ...$Y need to install $N"
+    fi
+
+done
