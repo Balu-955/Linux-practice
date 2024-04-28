6ae62ef73213b44a21c9aed0cf3f56e2ee263872
Siva837453 <sivaallaka777@gmail.com>
2024-04-28 17:37:14 +0530

    data types

--- 14.current-script.sh
+++ 14.current-script.sh
@@ -5,6 +5,8 @@ COURSE="Devops for current script"
 echo process id for current script PID: $$
 echo "Before calling current script couse: $COURSE"
 
-./15.other-script.sh
+#./15.other-script.sh
+
+sourse./15.other-script.sh
 
 echo "after calling other script course: $COURSE"
