cf993f93c589a5d2d1b9360e51b04406ef39b108
Siva837453 <sivaallaka777@gmail.com>
2024-04-28 17:51:41 +0530

    data types

--- 14.current-script.sh
+++ 14.current-script.sh
@@ -7,6 +7,6 @@ echo "Before calling current script couse: $COURSE"
 
 #./15.other-script.sh
 
-sourse./15.other-script.sh
+source ./15.other-script.sh
 
 echo "after calling other script course: $COURSE"
