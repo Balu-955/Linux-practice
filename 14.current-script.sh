e55206ab9f5857395cb7980974e77bf026004cd1
Siva837453 <sivaallaka777@gmail.com>
2024-04-28 17:27:46 +0530

    data types

--- 14.current-script.sh
+++ 14.current-script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-COURSE="Dvops for current script"
+COURSE="Devops for current script"
 
 echo process id for current script PID: $$
 echo "Before calling current script couse: $COURSE"
 
-#./15.other-script.sh
+./15.other-script.sh
 
 echo "after calling other script course: $COURSE"
--- 15.other-script.sh
+++ 15.other-script.sh
@@ -2,5 +2,5 @@
 
 COURSE="Devops from other script"
 
-echo " variab;e value for other script course:$COURSE"
+echo " variable value for other script course:$COURSE"
 echo "process id for other script PID : $$"
